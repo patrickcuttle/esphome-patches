b2e45834c96b3aa32e4c88f91e40797435974289
patrickcuttle <40672447+patrickcuttle@users.noreply.github.com>
2026-09-21 18:49:23 -0400

    Fix get_dimensions unpack: mainline ESPHome now returns 6 values (added pad_width/pad_height), was still expecting 4

--- esphome/components/mipi_dsi/display.py
+++ esphome/components/mipi_dsi/display.py
@@ -199,7 +199,9 @@ async def to_code(config):
     model = MODELS[config[CONF_MODEL].upper()]
     color_depth = COLOR_DEPTHS[get_color_depth(config)]
     pixel_mode = int(config[CONF_PIXEL_MODE].removesuffix("bit"))
-    width, height, _offset_width, _offset_height = model.get_dimensions(config)
+    width, height, _offset_width, _offset_height, _pad_width, _pad_height = (
+        model.get_dimensions(config)
+    )
     var = cg.new_Pvariable(config[CONF_ID], width, height, color_depth, pixel_mode)
 
     sequence, madctl = model.get_sequence(config)
